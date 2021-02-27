0ddacfc6b0fc7f7b89f8f6741b5a6b7a9c355337
Ram <kittuvelagala@gmail.com>
2021-02-27 11:21:43 -0600

    Initial commit

---
+++ avro_schema.py
@@ -0,0 +1,233 @@
+from .data_elements_query import DataQuery,data_main,data_meta
+
+
+
+def schema():
+
+    query_data = DataQuery(data_main, data_meta)
+    gen_format_items = query_data.format_generation()
+
+    data = {
+        "name": "Report",
+        "properties":{
+            "host_name": query_data.host_name,
+            "Data Store Name": "",
+            "Database Name": query_data.db_name,
+            "Table Name": query_data.table_name
+        },
+
+        "type": "record",
+
+        "fields": [
+            {
+                "name": "Column",
+                "type": "string",
+                "values": "",
+            },
+            {
+                "name": "inferred_data_class",
+                "type": "string",
+                "values": "",
+            },
+
+            #metadata
+            {
+                "name": "MetaData",
+                "type": {
+                    "name": "MetaData",
+                    "type": "record",
+                    "fields": [
+                        {
+                            "name": "Inferred",
+                            "type": {
+                                "name": "Inferred",
+                                "type": "record",
+                                "fields": [
+                                    {
+                                        "name": "Data_type",
+                                        "type": "string",
+                                        "values": "",
+                                    },
+                                    {
+                                        "name": "data_length",
+                                        "type": "int",
+                                        "values": "",
+                                    },
+                                    {
+                                        "name": "Precision",
+                                        "type": "int",
+                                        "values": query_data.num_prec,
+                                    },
+                                    {
+                                        "name": "scale",
+                                        "type": "int",
+                                        "values": "",
+                                    },
+                                    {
+                                        "name": "data_scale",
+                                        "type": "int",
+                                        "values": "",
+                                    }
+                                ]
+                            }
+                        },
+                        {
+                            "name": "Defined",
+                            "type": {
+                                "name": "Defined",
+                                "type": "record",
+                                "fields": [
+                                    {
+                                        "name": "Data_type",
+                                        "type": "string",
+                                        "values": "",
+                                    },
+                                    {
+                                        "name": "data_lenght",
+                                        "type": "int"
+                                    },
+                                    {
+                                        "name": "Precision",
+                                        "type": "int"
+                                    },
+                                    {
+                                        "name": "scale",
+                                        "type": "int"
+                                    },
+                                    {
+                                        "name": "data_scale",
+                                        "type": "int"
+                                    }
+                                ]
+                            }
+                        },
+                        {
+                            "name": "Defined",
+                            "type": {
+                                "name": "Defined",
+                                "type": "record"
+                            }
+                        }
+                    ]
+                }
+            },
+            # Domain Higlights
+            {
+                "name": "domain_highlights",
+                "type": {
+                    "name": "domain_highlights",
+                    "type": "record",
+                    "fields": [
+                        {
+                            "name": "TotalRows",
+                            "type": "int",
+                            "values": query_data.total_rows,
+                        },
+                        {
+                            "name": "Cardinality",
+                            "type": "int",
+                            "values": query_data.cardinality,
+                        },
+                        {
+                            "name": "per_of_Cardinality",
+                            "type": "int",
+                            "values": query_data.perc_of_cardinality,
+                        },
+                        {
+                            "name": "Nulls",
+                            "type": "int",
+                            "values": query_data.num_null_values,
+                        },
+                        {
+                            "name": "no_of_general_formats",
+                            "type": "int",
+                            "values": len(gen_format_items),
+                        },
+
+                    ]
+                },
+                "properties": {
+
+                    {
+                        "name": "general_formats",
+                        "type": "array",
+                        "values": gen_format_items,
+
+
+                    },
+                    {
+                        "name": "Lowest Value",
+                        "type": "string",
+                        "values": query_data.lowest_value,
+                    },
+                    {
+                        "name": "Highest Value",
+                        "type": "string",
+                        "values": query_data.highest_value,
+                    },
+                    {
+                        "name": "Median Value",
+                        "type": "string",
+                        "values": query_data.median_value,
+                    },
+                    {
+                        "name": "Least Frequent Value",
+                        "type": "string",
+                        "values": query_data.freq_lowest_value,
+                    },
+                    {
+                        "name": "Lowest Frequency",
+                        "type": "string",
+                        "values": query_data.freq_lowest_count,
+                    },
+                    {
+                        "name": "Lowest Frequency percentage",
+                        "type": "string",
+                        "values": query_data.freq_lowest_perc,
+                    },
+                    {
+                        "name": "Highest Frequent Value",
+                        "type": "string",
+                        "values": query_data.freq_highest_value_value,
+                    },
+                    {
+                        "name": "Highest Frequency",
+                        "type": "string",
+                        "values": query_data.freq_highest_count_count,
+                    },
+                    {
+                        "name": "highest Frequency percentage",
+                        "type": "string",
+                        "values": query_data.freq_highest_perc,
+                    },
+
+                }
+            },
+
+            {
+                "name": "domain_values",
+                "type": "array",
+                "fields": [
+                    {
+                        "name": "top_25",
+                        "type": "array",
+                        "value": [],
+                    },
+                    {
+                        "name": "bottom_25",
+                        "type": "array",
+                        "value": [],
+                    }
+                ],
+
+            },
+
+        ]
+    }
+
+
+    return data
+
+
+
+print(schema())
---
+++ data_element_query.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import re
+from .utils import sql_connector
+
+
+def connect_netezza(query):
+    """
+    Tests netezza connections
+    """
+    netezza = sql_connector('NETEZZA')
+    df = pd.read_sql(query, netezza)
+    return df
+
+
+query1 = "select * from example database "
+query_main_test = "select UNIQ_ACCT_IND_QTY FROM PBKDMDB.ADMIN.M_BRK_FC_CC_VTG_AGG_MLY LIMIT 100;"
+query_main = "select UNIQ_ACCT_IND_QTY FROM PBKDMDB.ADMIN.M_BRK_FC_CC_VTG_AGG_MLY ;"
+query_metadata_defined = """SELECT NUMERIC_PRECISION, NUMERIC_SCALE, IS_NULLABLE FROM INFORMATION_SCHEMA.COLUMNS WHERE(TABLE_NAME = 'DBKDMDB.ADMIN.M_BRK_FC_CC_VTG_AGG_MLY') AND(COLUMN_NAME = 'CC_VTG_AGG_MLY_SK');"""
+data_main = connect_netezza(query_main)
+data_meta = connect_netezza(query_metadata_defined)
+
+
+class DataQuery:
+
+
+    def __init__(self, data_main,data_meta):
+        self.total_rows = len(data_main.axes[0])
+        self.cardinality = len(pd.unique(data_main))
+        self.perc_of_cardinality = (self.cardinality / self.total_rows) * 100
+        self.num_null_values = data_main.isnull().sum()
+        self.min_value = data_main.min()
+        self.max_value = data_main.max()
+        self.median_value = data_main.median()
+        self.freq_highest_value = data_main.dropna().value_counts().idxmax()
+        self.freq_highest_count = data_main.dropna().value_counts().max()
+        self.freq_highest_perc = self.freq_highest_count / self.total_rows
+        self.freq_lowest_value = data_main.dropna().value_counts().idxmin()
+        self.freq_lowest_count = data_main.dropna().value_counts().min()
+        self.freq_lowest_perc = self.freq_lowest_count / self.total_rows
+        self.lowest_value = data_main.min()
+        self.highest_value = data_main.max()
+        self.median_value = data_main.median()
+        self.num_prec = data_meta["NUMERIC_PRECISION"]
+        self.num_scale = data_meta["NUMERIC_SCALE"]
+        self.is_nullable = data_meta["IS_NULLABLE"]
+        #top 25% unique values
+        #bottom 25% unique values
+        self.deta_meta2 = self.deta_meta.drop_duplicates()
+        # self.top_25%_unique = data_meta2.
+
+    def format_generation(self):
+        res_list = []
+        data1 = [str(num) for num in data_main]
+        for num in data1:
+            a = num.split('.')
+            x = ''
+            y = ''
+            dd = re.sub(r'[^\w]', '', a[0])
+            x = re.sub(r"\d+", len(dd) * '9', dd)
+            # print(x)
+            if len(a) == 2:
+                y = re.sub(r"\d+", len(a[1]) * '9', dd)
+                y = '.' + y
+            else:
+                pass
+            if a[0].startswith('-'):
+                x = '-' + x
+            res = x + y
+            res_list.append(res)
+        return data1, res_list
+
+    def json_generation(self):
+        data_set = data = {'metatadata':
+                               {'length': '', 'precision': '',
+                                }
+                           }
+
+
+if __name__ == '__main__':
+    dataquery = DataQuery
+    dataquery.connect_netezza(query1)
+
