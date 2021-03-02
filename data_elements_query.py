9cae64ab1429261968bc5de2179c1e3444c9bc9a
Ram velagala <67812401+ramvelagala@users.noreply.github.com>
2021-03-01 19:33:21 -0600

    Update data_elements_query.py
    march -1-copying

--- data_elements_query.py
+++ data_elements_query.py
@@ -1,42 +1,31 @@
+"""Data-profiling data-generator."""
+
 import pandas as pd
 import re
-import os
-
-print(os.getcwd())
-df = pd.read_csv("test_values.csv")
-data_main = df["loan_amount"]
-# print(df.head())
-#one column -data-main
-col_names = list(df.columns)
+from .utils import sql_connector
 
 
-
-
-# from .utils import sql_connector
-
-'''
 def connect_netezza(query):
-    """
-    Tests netezza connections
-    """
+    """Test netezza connections."""
     netezza = sql_connector('NETEZZA')
     df = pd.read_sql(query, netezza)
     return df
 
 
-query1 = "select * from example database "
-query_main_test = "select UNIQ_ACCT_IND_QTY FROM PBKDMDB.ADMIN.M_BRK_FC_CC_VTG_AGG_MLY LIMIT 100;"
+query_main_test = "select UNIQ_ACCT_IND_QTY FROM PBKDMDB.ADMIN.M_BRK_FC_CC_VTG_AGG_MLY LIMIT 10;"
 query_main = "select UNIQ_ACCT_IND_QTY FROM PBKDMDB.ADMIN.M_BRK_FC_CC_VTG_AGG_MLY ;"
-query_metadata_defined = """SELECT NUMERIC_PRECISION, NUMERIC_SCALE, IS_NULLABLE FROM INFORMATION_SCHEMA.COLUMNS 
-WHERE(TABLE_NAME = 'DBKDMDB.ADMIN.M_BRK_FC_CC_VTG_AGG_MLY') AND(COLUMN_NAME = 'CC_VTG_AGG_MLY_SK');"""
+query_metadata_defined = "SELECT NUMERIC_PRECISION, NUMERIC_SCALE, IS_NULLABLE FROM INFORMATION_SCHEMA.COLUMNS WHERE(" \
+                         "TABLE_NAME = 'DBKDMDB.ADMIN.M_BRK_FC_CC_VTG_AGG_MLY') AND(COLUMN_NAME = " \
+                         "'CC_VTG_AGG_MLY_SK'); "
 data_main = connect_netezza(query_main)
 data_meta = connect_netezza(query_metadata_defined)
-'''
 
 
 class DataQuery:
+    """Class to get required fields in data-profiling."""
 
     def __init__(self):
+        """Init to to generate desired fields."""
         self.total_rows = len(data_main.axes[0])
         self.cardinality = len(pd.unique(data_main))
         self.perc_of_cardinality = (self.cardinality / self.total_rows) * 100
@@ -53,16 +42,19 @@ class DataQuery:
         self.lowest_value = data_main.min()
         self.highest_value = data_main.max()
         self.median_value = data_main.median()
-        # self.num_prec = data_meta["NUMERIC_PRECISION"]
-        # self.num_scale = data_meta["NUMERIC_SCALE"]
-        # self.is_nullable = data_meta["IS_NULLABLE"]
-        name = list(data_main.columns)[0]
-        n = int(len(df)/4)
-        self.bot_25_perc_unique = data_main.nsmallest(n, list(data_main.columns)[0]).iloc[:, 0].tolist()
-        self.top_25_perc_unique = data_main.nlargest(n, list(data_main.columns)[0]).iloc[:, 0].tolist()
+        self.num_prec = data_meta["NUMERIC_PRECISION"]
+        self.num_scale = data_meta["NUMERIC_SCALE"]
+        self.is_nullable = data_meta["IS_NULLABLE"]
+        self.count_err = data_meta.where(data_meta == -1).count()
+        self.count_err_perc = self.count_err/self.total_rows
+        # num = data_main.columns[0]
+        # n = int(len(df)/4)
+        # self.bot_25_perc_unique = data_main.nsmallest(n, num).iloc[:, 0].tolist()
+        # self.top_25_perc_unique = data_main.nlargest(n, num).iloc[:,0].tolist()
 
     @staticmethod
     def format_generation():
+        """Generate general format of values in column."""
         res_list = []
         data1 = [str(num) for num in data_main]
         for num in data1:
@@ -70,7 +62,6 @@ class DataQuery:
             y = ''
             dd = re.sub(r'[^\w]', '', a[0])
             x = re.sub(r"\d+", len(dd) * '9', dd)
-            # print(x)
             if len(a) == 2:
                 y = re.sub(r"\d+", len(a[1]) * '9', dd)
                 y = '.' + y
@@ -83,15 +74,5 @@ class DataQuery:
         return list(set(res_list))
 
 
-'''
-    def json_generation(self):
-        data_set = data = {'metatadata':
-                               {'length': '', 'precision': '',
-                                }
-                          }
-    
-
 if __name__ == '__main__':
-    dataquery = DataQuery
-    dataquery.connect_netezza(query1)
-'''
+    connect_netezza(data_main)
