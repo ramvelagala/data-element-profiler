c0c6eb9b1792b8bc11d57bd32ec83eecd15c9cdb
Ram velagala <67812401+ramvelagala@users.noreply.github.com>
2023-10-18 16:32:40 -0500

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,5 +1,26 @@
-formatted_dates = np.array([dt.strftime('%Y-%m-%d %H:%M:%S') for dt in datetime_array])
+import pandas as pd
+import numpy as np
 
-# Print the formatted dates
-for date in formatted_dates:
-    print(date)
+# Sample DataFrame
+data = {'date': ['2023-10-01', '2023-10-01', '2023-10-01', '2023-10-02', '2023-10-02'],
+        'classification': ['intime', 'intime', 'oot', 'intime', 'oot']}
+df = pd.DataFrame(data)
+
+# Group by date and apply your conditions
+def assign_split(group):
+    if 'oot' in group['classification'].values:
+        group['split'] = 'validation'
+    else:
+        group_size = len(group)
+        train_size = int(0.8 * group_size)
+        test_size = group_size - train_size
+        group['split'] = np.concatenate([['train'] * train_size, ['test'] * test_size])
+    return group
+
+df = df.groupby('date').apply(assign_split)
+
+# Reset the index and drop the groupby index
+df = df.reset_index(drop=True)
+
+print(df)
+
