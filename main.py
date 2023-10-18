528b320697d8be13a7ec1cc24cdcc97e64ce22ea
Ram velagala <67812401+ramvelagala@users.noreply.github.com>
2023-10-18 13:24:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-def extract_pretty_transcript(x):
-    try:
-        transcript_data = json.loads(x)
-        pretty_transcripts = [item.get('pretty_transcript') for item in transcript_data['transcript'] if item.get('pretty_transcript') is not None]
-        return ' '.join(pretty_transcripts)
-    except (ValueError, KeyError):
-        return None
-
-# Apply the function to the 'TRANSCRIPT_TXT' column
-df['Pretty_Transcripts'] = df['TRANSCRIPT_TXT'].apply(extract_pretty_transcript)
-
-print(df)
+filtered_df = data_input.groupby('conversation_id').apply(lambda x: x.loc[x['transcript'].apply(len).idxmax()])
