a0f63af8e23ba4a60fc0c0b306ddc484f5b9d55e
Ram velagala <67812401+ramvelagala@users.noreply.github.com>
2023-10-19 20:23:05 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,2 @@
-first_row_explode["TRANSCRIPT_TXT"] = first_row_explode["TRANSCRIPT_TXT"].apply(lambda x: json.loads(x).get('transcript') if x is not "")
+first_row_explode[columns_to_extract] = first_row_explode["TRANSCRIPT_TXT"].apply(lambda x: pd.Series({key: x[key] for key in columns_to_extract if x and x != ""  else None}))
+
