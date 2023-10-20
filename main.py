cdb8c25022fb98dc5322ad6f26d9cd5d04b37cbf
Ram velagala <67812401+ramvelagala@users.noreply.github.com>
2023-10-19 19:52:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-filtered_df = data_input.groupby('conversation_id').apply(lambda x: x.loc[x['transcript'].apply(len).idxmax()])
+first_row_explode["TRANSCRIPT_TXT"] = first_row_explode["TRANSCRIPT_TXT"].apply(lambda x: json.loads(x).get('transcript') if x is not "")
